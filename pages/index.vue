b200b863aa5402556852ce4ede7a999786f1b3e3
ojikun <o-fukuda@usen-next.jp>
2020-04-20 23:14:14 +0900

    aaaa

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
     <o-home-section />
+    <o-about-section />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import OHomeSection from '~/components/OHomeSection.vue'
+import OAboutSection from '~/components/OAboutSection.vue'
 
 export default Vue.extend({
   components: {
+    OAboutSection,
     OHomeSection
   }
 })
 </script>
 
 <style>
-しね
 </style>
